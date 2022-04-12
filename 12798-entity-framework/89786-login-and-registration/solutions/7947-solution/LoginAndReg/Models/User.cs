1aa21b2bfdf8302fc5856398938bf2ad67cccc6a
Jessie de Leon <jdeleon@village88.com>
2022-04-13 03:50:36 +0800

    Copy module from 12798-entity-framework/89786-login-and-registration

---
+++ 12798-entity-framework/89786-login-and-registration/solutions/7947-solution/LoginAndReg/Models/User.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoginAndReg.Models
+{
+    public class MainUser
+    {
+        [Key]
+        public int UserId {get;set;}
+        [Display(Name="First Name")]
+        [MinLength(2)]
+        [Required]
+        public string FirstName {get;set;}
+        [Required]
+        [MinLength(2)]
+        [Display(Name="Last Name")]
+        public string LastName {get;set;}
+        [Required]
+        [EmailAddress]
+        public string Email {get;set;}
+        [Required]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        public string Password {get;set;}
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        [NotMapped]
+        public string Confirm {get;set;}
+        
+    }
+    public class LoginUser
+    {
+        [EmailAddress]
+        [Required]
+        [Display(Name="Email")]
+        public string EmailAttempt {get;set;}
+        [DataType(DataType.Password)]
+        [Display(Name="Password")]
+        public string PasswordAttempt {get;set;}
+    }
+
+}
